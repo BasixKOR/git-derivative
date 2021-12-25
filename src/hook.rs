b20249ce0db76ec4e50a55a1e1fb471c31d4ed08
Basix <basix@basix.tech>
2021-12-25 15:41:28 +0900

    Give executable permission to hook on POSIX

--- src/hook.rs
+++ src/hook.rs
@@ -1,5 +1,5 @@
+use std::fs::{File, Permissions};
 use std::io::Write;
-use std::fs::File;
 use std::path::Path;
 
 use thiserror::Error;
@@ -18,7 +18,14 @@ pub fn install_hook(path: &Path) -> Result<(), HookInstallationError> {
         return Err(HookInstallationError::AlreadyInstalled);
     }
     let mut file = File::create(hook_path).unwrap();
-	file.write_all(include_bytes!("../resources/hook.sh"))?;
+    file.write_all(include_bytes!("../resources/hook.sh"))?;
+
+    #[cfg(unix)] // Give execute permissions to the hook
+    {
+        use std::os::unix::fs::PermissionsExt;
+        file.set_permissions(Permissions::from_mode(0o555))?;
+    }
+
     file.sync_all()?;
-	Ok(())
+    Ok(())
 }
